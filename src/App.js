8c98d774ebc3528df360400c4e64ec25c2c546b2
SatwikaKacham <73744169+SatwikaKacham@users.noreply.github.com>
2022-05-10 20:51:45 +0530

    docs:horizontal-card-component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Header from "../src/components/header-component/HeaderComponent"
 import Footer from "../src/components/footer-component/FooterComponent"
+import HorizontalCard from "../src/components/horizontal-card-component/HorizontalCardComponent"
 function App() {
   return (
     <div className="App">
+
       <Header></Header>
+      <HorizontalCard></HorizontalCard>
       <Footer></Footer>
     </div>
   );
---
+++ src/components/horizontal-card-component/HorizontalCardComponent.js
@@ -0,0 +1,27 @@
+import "../horizontal-card-component/horizontal-card-component.css"
+import projectImg from  "../../images/project-banner-final.webp"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faArrowRightLong} from "@fortawesome/free-solid-svg-icons"
+export default function HorizontalCardComponent()
+{
+    return(
+        <div className="horizontal-card-container">
+                <div className="horizontal-card-content">
+                 <div className="horizontal-card-qoute" >
+                         We believe good design makes life beautiful
+                 </div>
+                 <div className="horizontal-card-title" >
+                        AALAYA
+                 </div>
+                 <div className="horizontal-card-location-container" >
+                     <div className="horizontal-card-location">
+                     Warangal-hyderabad highway
+                     </div>
+                     <FontAwesomeIcon icon={faArrowRightLong} className="card-right-arrow"></FontAwesomeIcon>
+                 </div>
+             </div>
+             <img src={projectImg} alt="project image" class="project-image"></img>
+         
+        </div>
+    )
+}
