e38a4be2b2fe668432cf187105db30c3cbbe93c7
SatwikaKacham <73744169+SatwikaKacham@users.noreply.github.com>
2022-05-10 12:55:45 +0530

    docs:header-component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Header from "../src/components/header-component/HeaderComponent"
 import Footer from "../src/components/footer-component/FooterComponent"
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Footer></Footer>
     </div>
   );
